27473eaa29b61db7880914f5c387dbde29a523cd
Mjsilva7 <mj.silva7@gmail.com>
2021-10-27 17:45:49 -0300

    Segundo commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextField from "ui/components/inputs/TextField/TextField";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,8 @@ const Home: NextPage = () => {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
+
+      <TextField Label={"Digite seu CEP"} fullwidth />
 
       <UserInformation
         name={"lorem Ipsum"}
