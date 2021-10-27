9b780885a0ecc19be2a12a1fc038214baabdb319
Mjsilva7 <mj.silva7@gmail.com>
2021-10-27 15:44:42 -0300

    primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+
+      <UserInformation
+        name={"lorem Ipsum"}
+        picture={"https://github.com/hanashiro.png"}
+        rating={3}
+        description={"Porto Seguro"}
+      />
+    </div>
+  );
+};
+
+export default Home;
