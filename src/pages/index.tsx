1df9f30f77341f3b0220e4da174d1afd18d506cd
Mjsilva7 <mj.silva7@gmail.com>
2021-10-28 11:24:10 -0300

    Segundo commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import type { NextPage } from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
-import TextField from "ui/components/inputs/TextField/TextField";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@mui/material";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
+import useIndex from "data/hooks/pages/useIndex.page";
 
 const Home: NextPage = () => {
+  const { cep, setCep } = useIndex();
   return (
     <div>
       <SafeEnvironment />
@@ -14,15 +22,61 @@ const Home: NextPage = () => {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+            value={cep}
+            onChange={(event) => setCep(event.target.value)}
+          />
+          <Typography color={"error"}>CEP inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
 
-      <TextField Label={"Digite seu CEP"} fullwidth />
-
-      <UserInformation
-        name={"lorem Ipsum"}
-        picture={"https://github.com/hanashiro.png"}
-        rating={3}
-        description={"Porto Seguro"}
-      />
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"lorem Ipsum"}
+              picture={"https://github.com/hanashiro.png"}
+              rating={3}
+              description={"Porto Seguro"}
+            />
+            <UserInformation
+              name={"lorem Ipsum"}
+              picture={"https://github.com/hanashiro.png"}
+              rating={3}
+              description={"Porto Seguro"}
+            />
+            <UserInformation
+              name={"lorem Ipsum"}
+              picture={"https://github.com/hanashiro.png"}
+              rating={3}
+              description={"Porto Seguro"}
+            />
+            <UserInformation
+              name={"lorem Ipsum"}
+              picture={"https://github.com/hanashiro.png"}
+              rating={3}
+              description={"Porto Seguro"}
+            />
+            <UserInformation
+              name={"lorem Ipsum"}
+              picture={"https://github.com/hanashiro.png"}
+              rating={3}
+              description={"Porto Seguro"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 };
